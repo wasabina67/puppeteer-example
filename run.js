e4a1b80820bbb7aa86dd9c58bec8d03b1db55e05
wasabina67 <wasabina67@gmail.com>
2024-07-14 14:08:38 +0900

    Update run.js

--- run.js
+++ run.js
@@ -8,5 +8,19 @@ const url = 'https://example.com/';
   await page.goto(url);
   await page.screenshot({ path: 'images/default.png' });
 
+  const devices = [
+    { name: 'iPhone SE', fileName: 'iphone-se.png' },
+    { name: 'iPhone 13 Pro', fileName: 'iphone-13-pro.png' },
+    { name: 'iPad Pro 11', fileName: 'ipad-pro-11.png' },
+  ];
+  for (const device of devices) {
+    const dev = puppeteer.KnownDevices[device.name];
+    await page.emulate(dev);
+    await page.goto(url);
+    await page.screenshot(
+      { path: 'images/' + device.fileName, fullPage: true }
+    );
+  }
+
   await browser.close();
 })();
